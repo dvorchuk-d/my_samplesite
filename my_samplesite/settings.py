7b1998191f86f4caa2893e19137cc199bc3f6251
Danil Dvorchuk <dvorchyk.d@gmail.com>
2022-01-10 19:51:40 +0200

    Changed SQLite to PostgreSQL

--- my_samplesite/settings.py
+++ my_samplesite/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'my_samplesite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_samplesite_db',
+        'USER': 'my_samplesite',
+        'PASSWORD': 'my_samplesite',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
